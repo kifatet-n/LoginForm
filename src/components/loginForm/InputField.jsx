8296fad8360e9a2aab6d438aa07625adf591eac8
kifatet-n <n.kifayet17@gmail.com>
2025-03-14 00:22:19 +0400

    handleSubmit

--- src/components/loginForm/InputField.jsx
+++ src/components/loginForm/InputField.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { MdOutlineEmail,  } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
 import { CiLock } from "react-icons/ci";
 
-
-const InputField = ({ label, type, placeholder }) => {
+const InputField = ({ label, type, placeholder, value, onChange, error }) => {
+  console.log("value:", value);
+  console.log("onChange:", onChange);
   //iconu input novune gore teyin edirik
   const getIcon = () => {
     if (type === "email") return <MdOutlineEmail className="input-icon" />;
@@ -14,8 +15,15 @@ const InputField = ({ label, type, placeholder }) => {
     <div className="input-group">
       <label className="input-label">{label}</label>
       <div className="input-wrapper">
-      {getIcon()}
-      <input type={type} placeholder={placeholder} />
+        {getIcon()}
+        <input
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={error ? "input-error" : ""}
+        />
+        {error && <span className="error-message">{error}</span>}
       </div>
     </div>
   );
--- src/components/loginForm/Login.jsx
+++ src/components/loginForm/Login.jsx
@@ -1,39 +1,74 @@
-import React from 'react'
-import InputField from './InputField';
-import Checkbox from './Checkbox';
-import Button from './Button';
+import React, { useState } from "react";
+import InputField from "./InputField";
+import Checkbox from "./Checkbox";
 import "./styles.css";
 
-
 const Login = () => {
-    const handleSubmit =(event)=>{
-        event.preventDefault();
-        console.log("Form submitted!");
-    };
-    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessages, setErrorMessages] = useState({});
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("login duymesine basildi");
+    let errors = {};
+    //email yoxlanisi
+    if (!email.includes("@")) {
+      errors.email = "Email duzgun deyil! '@' isaresinden istifade etmelisen";
+    }
+    //password yoxlanisi
+    if (password.length < 6) {
+      errors.password = "Sifre en azi 6 simvol olmalidir!";
+    }
+    console.log("Errors:", errors);
+    //Eger error varsa onlari goster
+    setErrorMessages(errors);
+
+    if (Object.keys(errors).length === 0) {
+      console.log("giris ugurlu oldu");
+    }
+  };
 
   return (
     <div className="login-container">
-        <button className="logo-btn">Logo</button>
-        <h2>Log In</h2>
-        <p>Enter your login details</p>
-        <form onSubmit={handleSubmit}>
-            
-
-            <InputField label="Email" type="email" placeholder="Enter your email" />
-            
-            <InputField label="Password" type="password" placeholder="Enter your password"/>
-            <div className="options">
-                <Checkbox label="Remember Me"/>
-                <a href="#">Forgot Password?</a>
-            </div>
-            <Button text="Log In"/>
-        </form>
-        <p>Guide book</p>
-        <p>
-            Dont have an account?<a href="#">Sign Up</a>
-        </p>
-
+      <button className="logo-btn">Logo</button>
+      <h2>Log In</h2>
+      <p>Enter your login details</p>
+      <form onSubmit={handleSubmit}>
+        <InputField
+          label="Email"
+          type="email"
+          value={email}
+          OnChange={handleEmailChange}
+          error={errorMessages.email}
+          placeholder="Enter your email"
+        />
+        {/* {errorMessages.email && <p>{errorMessages.email}</p>} */}
+        <InputField
+          label="Password"
+          type="password"
+          value={password}
+          OnChange={handlePasswordChange}
+          error={errorMessages.password}
+          placeholder="Enter your password"
+        />
+        {/* {errorMessages.password && <p>{errorMessages.password}</p>} */}
+        <div className="options">
+          <Checkbox label="Remember Me" />
+          <a href="#">Forgot Password?</a>
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+      <p>Guide book</p>
+      <p>
+        Dont have an account?<a href="#">Sign Up</a>
+      </p>
     </div>
   );
 };
